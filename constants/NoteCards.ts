acc5407296715a63764d158348e149239103c5e2
shafu <shafu0x@gmail.com>
2024-10-15 16:33:14 +0200

    update extension description

--- constants/NoteCards.ts
+++ constants/NoteCards.ts
@@ -3,14 +3,14 @@ export const MAX_DEPOSIT_VAULTS = 5;
 export const NOTE_EXTENSIONS = [
   {
     label: "Native ETH",
-    description: "Deposit ETH into your note",
+    description: "Deposit native ETH into your note without manually wrapping it",
     address: "0xDc40",
     enabled: false,
     icon: "/3d-eth.png",
   },
   {
     label: "Redemption",
-    description: "Redeem DYAD for underlying collateral",
+    description: "Redeem DYAD directly for underlying collateral",
     address: "0xDc40",
     enabled: false,
     icon: "/redeem.png",
