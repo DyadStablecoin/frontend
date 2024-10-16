27b2b3042b310dcad1746bfd75451fea6564703d
Mostafa Mahmoud <mostafamahmoud24@gmail.com>
2024-10-16 17:11:55 +0300

    conditional render of no notes only when wallet is connected

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ const EarnKeroseneContent = dynamic(
 );
 
 export default function Home() {
-  const { address } = useAccount();
+  const { address, isConnected } = useAccount();
   const searchParams = useSearchParams();
   const tab = searchParams.get("tab");
 
@@ -43,16 +43,14 @@ export default function Home() {
         <ClaimModalContent />
       </div>
       <div className="flex flex-col gap-4">
-        {tokens && tokens?.length ? (
-          tokens.map((token) => (
-            <NoteCard
-              key={parseInt(token.result)}
-              tokenId={parseInt(token.result)}
-            />
-          ))
-        ) : (
-          <NoNotesAvailable />
-        )}
+        {tokens && tokens?.length
+          ? tokens.map((token) => (
+              <NoteCard
+                key={parseInt(token.result)}
+                tokenId={parseInt(token.result)}
+              />
+            ))
+          : isConnected && <NoNotesAvailable />}
       </div>
     </>
   );
