58ab3b177acb57a890045b4f9bc1d9c43d8beb2c
shafu <shafu0x@gmail.com>
2024-10-26 19:16:04 +0200

    rename

--- constants/Stake.ts
+++ constants/Stake.ts
@@ -2,11 +2,11 @@ import { StakeCurrencies, StakeContractsType } from "@/models/Stake";
 
 export const STAKE_CONTRACTS: StakeContractsType = {
   [StakeCurrencies.CURVE_M0_DYAD_LP]: {
-    label: "M0 - DYAD",
+    label: "wM - DYAD",
     stakeKey: StakeCurrencies.CURVE_M0_DYAD_LP,
     address: "0xa969cFCd9e583edb8c8B270Dc8CaFB33d6Cf662D",
     stakingContract: "0xe48c80CF20C7fCE3458896BB263D9D8D6404b39f",
-    name: "M0/DYAD LP",
+    name: "wM/DYAD LP",
   },
   [StakeCurrencies.ETH]: {
     label: "ETH - DYAD",
