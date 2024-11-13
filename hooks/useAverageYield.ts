99f39d705aed5d6eb81edb9f277f5d558c4d23ad
shafu <shafu0x@gmail.com>
2024-11-14 02:22:53 +0700

    kero price

--- hooks/useAverageYield.ts
+++ hooks/useAverageYield.ts
@@ -1,8 +1,10 @@
 import { useMemo, useState, useEffect } from "react";
 import { useQuery, gql } from "@apollo/client";
+import useKerosenePrice from "./useKerosenePrice";
 
 export default function useAverageYield() {
     const [averageYield, setAverageYield] = useState(0);
+    const [totalLiquidity, setTotalLiquidity] = useState(0);
 
     const GET_REWARDS = gql`
         query {
@@ -15,22 +17,42 @@ export default function useAverageYield() {
         }
     `;
 
+    const {kerosenePrice} = useKerosenePrice();
     const { loading, error, data } = useQuery(GET_REWARDS, { fetchPolicy: 'network-only' });
 
     useEffect(() => {
         if (data && data.rewards && data.rewards.items.length > 0) {
-            const totalAmount = data.rewards.items.reduce((sum, item) => {
+            const totalRewards = data.rewards.items.reduce((sum, item) => {
                 const amount = Number(item.amount);
                 return sum + (isNaN(amount) ? 0 : amount); // Ensure only valid numbers are summed
             }, 0);
 
-            console.log('totalAmount', totalAmount);
+            console.log('totalRewards', totalRewards * kerosenePrice /  totalLiquidity);
+            console.log('kero', kerosenePrice);
 
-            const average = totalAmount / data.rewards.items.length;
-            console.log('totalAmount', average);
-            setAverageYield(average);
         }
-    }, [data]);
+    }, [data, totalLiquidity]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await fetch('https://api.dyadstable.xyz/api/yields/0');
+            const data = await response.json();
+            console.log("SSSS", data); // Handle the fetched data as needed
+            
+            // New code to sum totalLiquidity
+            const totalLiquidityValue = Object.values(data).reduce((sum, item) => {
+                const liquidity = Number(item.totalLiquidity);
+                return sum + (isNaN(liquidity) ? 0 : liquidity); // Ensure only valid numbers are summed
+            }, 0);
+            setTotalLiquidity(totalLiquidityValue); // Set the totalLiquidity state
+          } catch (error) {
+            console.error("Error fetching data:", error);
+          }
+        };
+    
+        fetchData();
+      }, []);
 
     return { averageYield, loading, error };
 }
