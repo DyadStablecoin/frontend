0a641ed089365b1acedf6a400e914434e1a1c2a3
shafu <shafu0x@gmail.com>
2024-11-14 02:57:03 +0700

    rm unused code

--- components/ui/footer.tsx
+++ components/ui/footer.tsx
@@ -1,9 +1,6 @@
-"use client";
 import Link from "next/link";
 
 import { cn } from "@/lib/utils";
-import useAverageYield from "@/hooks/useAverageYield";
-import { useState } from "react";
 
 export function Footer({
   className,
